e870d3c93cfa10b0e057eaac479fe7260bb4880c
David Graham <david.malcom.graham@gmail.com>
2018-04-10 14:27:56 -0600

    Combine rollup config files

--- rollup.config.js
+++ rollup.config.js
@@ -6,10 +6,16 @@ const pkg = require('./package.json')
 
 export default {
   input: 'src/index.js',
-  output: {
-    file: pkg['module'],
-    format: 'es'
-  },
+  output: [
+    {
+      file: pkg['module'],
+      format: 'es'
+    },
+    {
+      file: pkg['main'],
+      format: 'umd'
+    }
+  ],
   plugins: [
     babel({
       plugins: ['transform-custom-element-classes'],
